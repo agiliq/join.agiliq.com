a1503178cad3f99eb016562656f7bff26bf8c5f3
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2012-10-11 17:21:50 +0530

    added docstrings to test cases

--- api/tests.py
+++ api/tests.py
@@ -6,6 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 import json
 import urlparse
+import StringIO
 
 from django.test import TestCase, client
 from django.conf import settings
@@ -20,6 +21,9 @@ class APIViewsTestCase(TestCase):
     Test cases for 'api.views'
     """
     def setUp(self):
+        """
+        initializes required variables to perform tests.
+        """
         user = User.objects.create_user("testuser@example.com",
                                         password="password",
                                         email="testuser@example.com")
@@ -37,7 +41,8 @@ class APIViewsTestCase(TestCase):
                                 token="abcd"
                             )
         
-        f = open('resume.pdf', 'r')
+        f = StringIO.StringIO("Contains Dummy Text")
+        f.name = "resume.pdf"
         self.data = { 'first_name':'FirstName',
                     'last_name' : 'LastName',
                     'projects_url':'http://www.example.com/name/projects/',
@@ -47,18 +52,25 @@ class APIViewsTestCase(TestCase):
 
     
     def test_resume_upload(self):
+        """
+        ``test_resume_upload`` will make a multipart post request. If this 
+        test Fails it raises an ``AssertionError``
+        """
         settings.JOB_MANAGERS = []
         resp = self.client.post(
                 '/api/resume/upload/?access_token=%s' % \
                     self.access_token.token,
-                self.data, 
-                #content_type='MULTIPART_CONTENT'
+                self.data
                 )
         self.assertEqual(resp.status_code, 200)
         resp_content = json.loads(resp.content)['success']
         self.assertEqual(resp_content, True)
         
     def test_resume_upload_missing_access_token(self):
+        """
+        ``test_resume_upload_missing_access_token`` will send the data missing 
+        access token. It raises ``AssertionError``s if this fails.
+        """
         resp = self.client.post('/api/resume/upload/', self.data)
         self.assertEqual(resp.status_code, 302)
         parsed_url = urlparse.urlparse(resp['Location'])
@@ -67,11 +79,18 @@ class APIViewsTestCase(TestCase):
         self.assertEqual(resp_dict['error'], ["invalid_request"])
         
     def test_resume_upload_invalid_access_token(self):
+        """
+        ``test_resume_upload_invalid_access_token`` will send an invalid access
+        token to the system.        
+        """
         resp = self.client.post('/api/resume/upload/?access_token=1234',\
                 self.data)
         self.assertEqual(resp.status_code, 302)
         
     def test_resume_upload_no_resume(self):
+        """
+        ``test_resume_upload_no_resume`` will test for the ``resume`` in data.
+        """
         temp_data = self.data.copy()
         del temp_data['resume']
         resp = self.client.post('/api/resume/upload/?access_token=%s' % \
@@ -80,6 +99,10 @@ class APIViewsTestCase(TestCase):
         self.assertEqual(resp_content, ["This field is required."])
         
     def test_resume_upload_invalid_projects_url(self):
+        """
+        ``test_resume_upload_invalid_projects_url`` will check for valid
+        projects url.
+        """
         temp_data = self.data.copy()
         temp_data['projects_url'] = 'abcdefg'
         resp = self.client.post('/api/resume/upload/?access_token=%s' % \
@@ -88,6 +111,9 @@ class APIViewsTestCase(TestCase):
         self.assertEqual(resp_content, ["Enter a valid URL."])
 
     def test_resume_upload_invalid_code_url(self):
+        """
+        ``test_resume_upload_invalid_code_url`` will check for valid code url.
+        """
         temp_data = self.data.copy()
         temp_data['code_url'] = 'abcdefg'
         resp = self.client.post('/api/resume/upload/?access_token=%s' % \
