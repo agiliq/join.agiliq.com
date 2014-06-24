0ec1910d0dd5af4451d3837379169bbf6d8eeae5
Ashik Sujath <ashik@agiliq.com>
2014-06-24 17:37:12 +0530

    production.py added to settings

--- jobs/settings/production.py
+++ jobs/settings/production.py
@@ -1,7 +1,19 @@
 from .base import *
 
-DEBUG = True
+import os
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ("Shabda Raaj", "shabda@agiliq.com"),
+    # ("Akshar Raaj", "akshar@agiliq.com"),
+    ("Ashik S", "ashik@agiliq.com"),
+)
+
+# JOB_MANAGERS = (
+#     "shabda@agiliq.com",
+# )
 
 DATABASES = {
     'default': {
@@ -15,6 +27,14 @@ DATABASES = {
 }
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
+ALLOWED_HOSTS = ['join.agiliq.com', '127.0.0.1']
+
+MEDIA_ROOT = SITE_PATH.child('media')
+MEDIA_URL = "/media/"
+
+STATIC_ROOT = SITE_PATH.child('static')
+STATIC_URL = "/static/"
+
 
 EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
@@ -23,3 +43,5 @@ EMAIL_HOST = get_env_variable('EMAIL_HOST')
 # Email settings
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+EMAIL_SUBJECT_PREFIX = "[Job]"
+DEFAULT_FROM_EMAIL = "hello@agiliq.com"
